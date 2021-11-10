435c076dd19fb8f678c215b53f42f0dfb8283c21
wangheng <630352167@qq.com>
2021-11-10 18:39:20 +0800

    更新react

--- vue3/代码/vue3_test/src/App.vue
+++ vue3/代码/vue3_test/src/App.vue
@@ -1,30 +1,74 @@
 <template>
-	<div class="app">
-		<h3>我是App组件</h3>
-		<Suspense>
-			<template v-slot:default>
-				<Child/>
-			</template>
-			<template v-slot:fallback>
-				<h3>稍等，加载中...</h3>
-			</template>
-		</Suspense>
-	</div>
+  <h1>一个人的信息</h1>
+  <h2>姓名：{{ name }}</h2>
+  <h2>年龄：{{ age }}</h2>
+  <h2>性别：{{ sex }}</h2>
+  <h2>a的值是：{{ a }}</h2>
+  <button @click="sayHello">说话(Vue3所配置的——sayHello)</button>
+  <br />
+  <br />
+  <button @click="sayWelcome">说话(Vue2所配置的——sayWelcome)</button>
+  <br />
+  <br />
+  <button @click="test1">测试一下在Vue2的配置中去读取Vue3中的数据、方法</button>
+  <br />
+  <br />
+  <button @click="test2">
+    测试一下在Vue3的setup配置中去读取Vue2中的数据、方法
+  </button>
 </template>
 
 <script>
-	// import Child from './components/Child'//静态引入
-	import {defineAsyncComponent} from 'vue' 
-	const Child = defineAsyncComponent(()=>import('./components/Child')) //异步引入
-	export default {
-		name:'App',
-		components:{Child},
-	}
-</script>
+// import { h } from "vue";
+export default {
+  name: "App",
+  data() {
+    return {
+      sex: "男",
+      a: 100,
+    };
+  },
+  methods: {
+    sayWelcome() {
+      alert("欢迎来到尚硅谷学习");
+    },
+    test1() {
+      console.log(this.sex);
+      console.log(this.name);
+      console.log(this.age);
+      console.log(this.sayHello);
+    },
+  },
+  //此处只是测试一下setup，暂时不考虑响应式的问题。
+  async setup() {
+    //数据
+    let name = "张三";
+    let age = 18;
+    let a = 200;
 
-<style>
-	.app{
-		background-color: gray;
-		padding: 10px;
-	}
-</style>
+    //方法
+    function sayHello() {
+      alert(`我叫${name}，我${age}岁了，你好啊！`);
+    }
+    function test2() {
+      console.log(name);
+      console.log(age);
+      console.log(sayHello);
+      console.log(this.sex);
+      console.log(this.sayWelcome);
+    }
+
+    //返回一个对象（常用）
+    return {
+      name,
+      age,
+      sayHello,
+      test2,
+      a,
+    };
+
+    //返回一个函数（渲染函数）
+    // return () => h("h1", "尚硅谷");
+  },
+};
+</script>
